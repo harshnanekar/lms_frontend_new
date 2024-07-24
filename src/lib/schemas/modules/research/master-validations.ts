d54d6e62a86de20dc4f4a5673b64dfef0e2768d1
harshnanekar <nanekarharshal2@gmail.com>
2024-07-25 00:45:42 +0530

    message

--- src/lib/schemas/modules/research/master-validations.ts
+++ src/lib/schemas/modules/research/master-validations.ts
@@ -305,7 +305,18 @@ export const EContentObj = z.object({
   })
 
   export const facultyObj = z.array(Faculty).min(1,{message:'Faculty details are required'});
-
   export type facultyReq = z.infer<typeof facultyObj>;
+
+  export const facultyUpd = z.object({
+	faculty_id  : z.number(),
+	faculty_name : z.string().min(1,{message:'Faculty name is required'}),
+    institute : z.string().min(1,{message :'Faculty institute name is required'}),
+	address : z.string().min(1,{message:'Faculty address is required'}),
+	designation : z.string().min(1,{message:'Faculty designation is required'}),
+    faculty_type : z.number().refine(data => data != 0,'Faculty type is required'),
+  })
+
+  export type facultyUpdReq = z.infer<typeof facultyUpd>;
+
 
 
