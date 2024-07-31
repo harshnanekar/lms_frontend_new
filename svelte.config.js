4e8d151fe0478f54844e361729ec0d6c7c8b4c5c
harshalnmims <harshal.nanekar.EXT@nmims.edu>
2024-07-31 16:39:52 +0530

    message

--- svelte.config.js
+++ svelte.config.js
@@ -6,6 +6,9 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
+	paths : {
+     base : process.env.BASE_URL
+	},
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
